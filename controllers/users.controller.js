1ed500b1392622d113be87fb14911e576d1d856a
ishshu <120395843+ishshu-23@users.noreply.github.com>
2024-01-15 21:52:42 +0530

    Update users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,13 +1,29 @@
 const { users } = require('../models/users.model.js');
+const { userCountriesIndexes } = require('../models/countries.model.js');
 const { getCountryIndex } = require('../controllers/country.controller.js');
 function addUser(res, userid, country) {
     var index = getCountryIndex(country);
-    console.log(index);
+    check = true;
+    for (let i = 0; i < userCountriesIndexes.length; i++) {
+        if(userCountriesIndexes[i].index === index){
+            userCountriesIndexes[i].quantity = userCountriesIndexes[i].quantity + 1;
+            check = false;
+            break;
+        }
+    }
+    if(check){
+        var obj = {
+            index: index,
+            quantity: 1
+        }
+        userCountriesIndexes.push(obj);
+    }
+    console.log(userCountriesIndexes);
+    
     users[index].push({
         userid: userid,
         res: res
     })
-    console.log(users);
 }
 
 module.exports = {
