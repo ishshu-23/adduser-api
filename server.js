5660822221d1da47842a298f62233da6d90df129
ishshu <120395843+ishshu-23@users.noreply.github.com>
2024-01-23 20:29:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.get("/hello", (req, res) => {
 app.use("/channel", channelRouter);
 
 setInterval(() => { 
-while(true){
     // console.log(userCountriesIndexes);
     var i1 = Math.floor(Math.random() * userCountriesIndexes.length);
     var i2 = Math.floor(Math.random() * userCountriesIndexes.length);
@@ -53,7 +52,7 @@ while(true){
             userCountriesIndexes.splice(inx, 1);
         }
     }
-}
+}, 1000);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
