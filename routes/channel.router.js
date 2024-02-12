08406ca4b3ebb3bc24c66249512ecd40bd0bc6fb
ishshu <120395843+ishshu-23@users.noreply.github.com>
2024-02-12 17:52:51 +0530

    Update channel.router.js

--- routes/channel.router.js
+++ routes/channel.router.js
@@ -1,23 +1,15 @@
-const { users } = require("../models/users.model.js");
-const { addUser } = require("../controllers/users.controller.js");
-
 const express = require("express");
+const { addUser } = require("../controllers/usersController");
 
 const channelRouter = express.Router();
 
 channelRouter.get("/", (req, res)=>{
-    res.json(
-        {
-            message: "Channel Route is working fine"
-        }
-    );
-})
+    res.json({ message: "Channel Route is working fine" });
+});
 
 channelRouter.get("/getchannelname/:userid/:gender/:country", (req, res)=>{
-    const userid = req.params.userid;
-    const country = req.params.country;
+    const { userid, country } = req.params;
     addUser(res, userid, country);
-    // console.log(users);
-})
+});
 
 module.exports = channelRouter;
