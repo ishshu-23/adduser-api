f2d5e16184e696ea790bf7912e7848c8385d9112
ishshu <120395843+ishshu-23@users.noreply.github.com>
2024-02-12 17:55:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -113,5 +113,5 @@ setInterval(() => {
     
 }, 100);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
